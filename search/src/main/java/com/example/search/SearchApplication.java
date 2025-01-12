644a10a88b12eb00f4e3738f8696e307a3dd1592
Franke Tang <franketang@gmail.com>
2025-01-12 04:53:32 -0600

    Add SearchService with University and Details

--- search/src/main/java/com/example/search/SearchApplication.java
+++ search/src/main/java/com/example/search/SearchApplication.java
@@ -3,9 +3,8 @@ package com.example.search;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.common", "com.example.search"})
 @EnableEurekaClient
 public class SearchApplication {
 
---
+++ search/src/main/java/com/example/search/service/SearchServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.search.service;
+
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class SearchServiceImpl implements SearchService{
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public SearchServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public Object[] search() {
+        CompletableFuture<String[]> universityFuture = CompletableFuture.supplyAsync(
+                () -> restTemplate.getForObject("http://localhost:9000/university", String[].class)
+        );
+        CompletableFuture<String> detailsFuture = CompletableFuture.supplyAsync(
+                () -> restTemplate.getForObject("http://localhost:9400/details/port", String.class)
+        );
+        CompletableFuture<Object[]> resultFuture = universityFuture.thenCombine(detailsFuture,
+                (university, details) -> {
+                    return Stream.concat(Stream.of(details), Arrays.stream(university)).toArray();
+                });
+        return resultFuture.join();
+    }
+}
